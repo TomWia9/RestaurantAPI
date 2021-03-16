14aa9ba4e51f6a42b8f1087f1daeec0eb147df0c
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-03-16 16:25:56 +0100

    Add database seeder

---
+++ RestaurantAPI/Models/RestaurantSeeder.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantAPI.Models
+{
+    public class RestaurantSeeder
+    {
+        private readonly RestaurantDbContext _context;
+
+        public RestaurantSeeder(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+        public void Seed()
+        {
+            if (_context.Database.CanConnect())
+            {
+                var pendingMigrations =  _context.Database.GetPendingMigrations();
+
+                if (pendingMigrations != null && pendingMigrations.Any())
+                {
+                    _context.Database.Migrate();
+                }
+
+                if (! _context.Restaurants.Any())
+                {
+                    var restaurants = GetRestaurants();
+
+                     _context.Restaurants.AddRange(restaurants);
+                     _context.SaveChanges();
+                }
+            }
+        }
+
+        private IEnumerable<Restaurant> GetRestaurants()
+        {
+            var restaurants = new List<Restaurant>()
+            {
+                new Restaurant()
+                {
+                    Name = "KFC",
+                    Category = "Fast Food",
+                    Description =
+                        "KFC (short for Kentucky Fried Chicken) is an American fast food restaurant chain headquartered in Louisville",
+                    ContactEmail = "contact@kfc.com",
+                    ContactNumber = "123456789",
+                    HasDelivery = true,
+                    Dishes = new List<Dish>()
+                    {
+                        new Dish()
+                        {
+                            Name = "Nashville Hot Chicken",
+                            Price = 10.30M,
+                        },
+
+                        new Dish()
+                        {
+                            Name = "Chicken Nuggets",
+                            Price = 5.30M,
+                        },
+                    },
+                    Address = new Address()
+                    {
+                        City = "Kraków",
+                        Street = "Długa 5",
+                        PostalCode = "30-001"
+                    }
+                },
+                new Restaurant()
+                {
+                    Name = "McDonald Szewska",
+                    Category = "Fast Food",
+                    Description =
+                        "McDonald's Corporation (McDonald's), incorporated on December 21, 1964, operates and franchises McDonald's restaurants.",
+                    ContactEmail = "contact@mcdonald.com",
+                    ContactNumber = "987654321",
+                    HasDelivery = true,
+                    Address = new Address()
+                    {
+                        City = "Kraków",
+                        Street = "Szewska 2",
+                        PostalCode = "30-001"
+                    }
+                }
+            };
+
+            return restaurants;
+        }
+    }
+}
--- RestaurantAPI/Startup.cs
+++ RestaurantAPI/Startup.cs
@@ -28,7 +28,7 @@ namespace RestaurantAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<RestaurantSeeder>();
             services.AddDbContext<RestaurantDbContext>
                 (options => options.UseSqlServer(Configuration.GetConnectionString("RestaurantDbConnection")));
 
@@ -40,8 +40,10 @@ namespace RestaurantAPI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, RestaurantSeeder seeder)
         {
+            seeder.Seed();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
