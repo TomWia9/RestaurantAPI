d7c0dacc88706c1dc168b0e5dd484617acc028c0
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-03-23 19:33:34 +0100

    Add dish validators

---
+++ RestaurantAPI/Shared/Validators/DishForCreationValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestaurantAPI.Data.Dto;
+
+namespace RestaurantAPI.Shared.Validators
+{
+    public class DishForCreationValidator : DishValidator<DishForCreationDto>
+    {
+    }
+}
---
+++ RestaurantAPI/Shared/Validators/DishValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using RestaurantAPI.Data.Dto;
+
+namespace RestaurantAPI.Shared.Validators
+{
+    public abstract class DishValidator<T> : AbstractValidator<T> where T: DishForManipulationDto
+    {
+        protected DishValidator()
+        {
+            RuleFor(d => d.Name).NotEmpty().Length(2, 30);
+            RuleFor(d => d.Description).NotEmpty().Length(5, 500);
+            RuleFor(d => d.Price).NotEmpty();
+
+        }
+    }
+}
