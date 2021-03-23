d3087b5688c112050c7873025234610a4a585622
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-03-23 16:58:22 +0100

    Add DishesController

---
+++ RestaurantAPI/Controllers/DishesController.cs
@@ -0,0 +1,102 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using RestaurantAPI.Data.Dto;
+using RestaurantAPI.Models;
+using RestaurantAPI.Repositories;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/Restaurants/{restaurantId}/[controller]")]
+    [ApiController]
+    public class DishesController : ControllerBase
+    {
+        private readonly IDishesRepository _dishesRepository;
+        private readonly IMapper _mapper;
+
+        public DishesController(IDishesRepository dishesRepository, IMapper mapper)
+        {
+            _dishesRepository = dishesRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DishDto>>> GetDishes(int restaurantId)
+        {
+            var dishes = await _dishesRepository.GetAllAsync(restaurantId);
+
+            return Ok(_mapper.Map<IEnumerable<DishDto>>(dishes));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<DishDto>>> GetDish(int restaurantId, int id)
+        {
+            var dish = await _dishesRepository.GetAsync(restaurantId, id);
+
+            if (dish == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<DishDto>(dish));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<DishDto>> NewDish(int restaurantId, DishForCreationDto dish)
+        {
+            var newDish = _mapper.Map<Dish>(dish);
+            newDish.RestaurantId = restaurantId;
+
+            if (await _dishesRepository.AddAsync(newDish))
+            {
+                return CreatedAtAction("GetDish", new {id = newDish.Id}, _mapper.Map<DishDto>(newDish));
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDish(int restaurantId, int id, DishForUpdateDto dish)
+        {
+            var dishFromRepo = await _dishesRepository.GetAsync(restaurantId, id);
+
+            if (dishFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(dish, dishFromRepo);
+
+            if (await _dishesRepository.UpdateAsync(dishFromRepo))
+            {
+                return NoContent();
+            }
+
+            return BadRequest();
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRestaurant(int restaurantId, int id)
+        {
+            var dish = await _dishesRepository.GetAsync(restaurantId, id);
+
+            if (dish == null)
+            {
+                return NotFound();
+            }
+
+            if (await _dishesRepository.DeleteAsync(dish))
+            {
+                return NoContent();
+            }
+
+            return BadRequest();
+
+        }
+    }
+}
