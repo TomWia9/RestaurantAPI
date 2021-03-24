fe050d94538d4a6ed4a372e846c23013b7284d47
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-03-24 23:11:45 +0100

    Add AuthController with signUp (Post) method

---
+++ RestaurantAPI/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using RestaurantAPI.Data.Resources;
+using RestaurantAPI.Models.Auth;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+
+        public AuthController(IMapper mapper, UserManager<User> userManager)
+        {
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+
+        [HttpPost("signup")]
+        public async Task<IActionResult> SignUp(UserSignUpResource userSignUpResource)
+        {
+            var user = _mapper.Map<UserSignUpResource, User>(userSignUpResource);
+
+            var userCreateResult = await _userManager.CreateAsync(user, userSignUpResource.Password);
+
+            if (userCreateResult.Succeeded)
+            {
+                return Created(string.Empty, string.Empty);
+            }
+
+            return Problem(userCreateResult.Errors.First().Description, null, 500);
+        }
+    }
+}
