ebb85571cbbb1b3a9b5243bb8cb48a8c4cb840fb
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-03-24 22:55:58 +0100

    Add UserSignUpResource and User mapper profile

---
+++ RestaurantAPI/Data/Profiles/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using RestaurantAPI.Data.Resources;
+using RestaurantAPI.Models.Auth;
+
+namespace RestaurantAPI.Data.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<UserSignUpResource, User>()
+                .ForMember(u => u.UserName, opt => opt.MapFrom(ur => ur.Email));
+        }
+    }
+}
