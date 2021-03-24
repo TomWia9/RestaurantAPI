75bbae997ba19520bc7e984a2f1da3a71e7cf32d
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-03-24 16:40:07 +0100

    Add Generic repository with bascic crud method

---
+++ RestaurantAPI/Repositories/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Repositories
+{
+    interface IGenericRepository<T> where T : class
+    {
+        Task AddAsync(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        Task<bool> SaveChangesAsync();
+    }
+}
