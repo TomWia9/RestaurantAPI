ecfcc97f2e1130e46aca48824ab09ddb1ae05028
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-04-01 19:38:43 +0200

    Add configuration for User entity

---
+++ RestaurantAPI/EntityConfiguration/UsersConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RestaurantAPI.Models.Auth;
+
+namespace RestaurantAPI.EntityConfiguration
+{
+    public class UsersConfiguration : IEntityTypeConfiguration<User>
+    {
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.Property(u => u.FirstName)
+            .IsRequired()
+            .HasMaxLength(30);
+
+        builder.Property(u => u.LastName)
+            .IsRequired()
+            .HasMaxLength(30);
+    }
+}
+}
--- RestaurantAPI/Migrations/20210401173722_AddUserConfiguration.Designer.cs
+++ RestaurantAPI/Migrations/20210401173722_AddUserConfiguration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RestaurantAPI.Models;
 
 namespace RestaurantAPI.Migrations
 {
     [DbContext(typeof(RestaurantDbContext))]
-    partial class RestaurantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210401173722_AddUserConfiguration")]
+    partial class AddUserConfiguration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
