7514d27e36209096b0e756da089833760ab16ca6
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-04-02 18:00:38 +0200

    Add RestaurantsResourceParameters class

---
+++ RestaurantAPI/Data/ResourceParameters/ResourceParameters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Data.ResourceParameters
+{
+    public abstract class ResourceParameters
+    {
+        public string SearchQuery { get; set; }
+    }
+}
---
+++ RestaurantAPI/Data/ResourceParameters/RestaurantsResourceParameters.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Data.ResourceParameters
+{
+    public class RestaurantsResourceParameters : ResourceParameters
+    {
+        public bool? HasDelivery { get; set; }
+    }
+}
