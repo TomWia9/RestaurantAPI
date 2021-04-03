8a0edb97945d74ee2d4bb92e3d6aa0f2506bc8c0
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-04-03 14:35:17 +0200

    Add sorting for GetAll dishes method

--- RestaurantAPI/Repositories/DishesRepository.cs
+++ RestaurantAPI/Repositories/DishesRepository.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using RestaurantAPI.Data.ResourceParameters;
 using RestaurantAPI.Models;
+using RestaurantAPI.Shared.Enums;
 using RestaurantAPI.Shared.PagedList;
 
 namespace RestaurantAPI.Repositories
@@ -13,11 +15,6 @@ namespace RestaurantAPI.Repositories
     {
         public DishesRepository(RestaurantDbContext context) : base(context)
         {
-        }
-
-        public async Task<IEnumerable<Dish>> GetAllAsync(int restaurantId)
-        {
-            return await _context.Dishes.Where(d => d.RestaurantId == restaurantId).ToListAsync();
         }
 
         public async Task<PagedList<Dish>> GetAllAsync(int restaurantId, DishesResourceParameters dishesResourceParameters)
@@ -44,13 +41,35 @@ namespace RestaurantAPI.Repositories
 
             }
 
-            return await PagedList<Dish>.ToPagedListAsync(collection.OrderBy(r => r.Name),
+            if (!string.IsNullOrWhiteSpace(dishesResourceParameters.SortBy))
+            {
+                collection = SortDishes(collection, dishesResourceParameters.SortBy, dishesResourceParameters.SortDirection);
+            }
+
+            return await PagedList<Dish>.ToPagedListAsync(collection,
                 dishesResourceParameters.PageNumber, dishesResourceParameters.PageSize);
         }
 
         public async Task<Dish> GetAsync(int restaurantId, int id)
         {
             return await _context.Dishes.FirstOrDefaultAsync(d => d.RestaurantId == restaurantId && d.Id == id);
+        }
+
+        private static IQueryable<Dish> SortDishes(IQueryable<Dish> collection, string sortBy, SortDirection sortDirection)
+        {
+            var columnsSelector = new Dictionary<string, Expression<Func<Dish, object>>>
+            {
+                { nameof(Dish.Name), d => d.Name },
+                { nameof(Dish.Description), d => d.Description },
+            };
+
+            var selectedColumn = columnsSelector[sortBy];
+
+            collection = sortDirection == SortDirection.ASC
+                ? collection.OrderBy(selectedColumn)
+                : collection.OrderByDescending(selectedColumn);
+
+            return collection;
         }
 
     }
