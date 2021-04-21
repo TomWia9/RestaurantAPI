c3b95b83c81faf443be845ba075a97dd3a21cf38
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-04-21 09:04:49 +0200

    Add PostRestaurant_Should_CreateNewRestaurant test

--- RestaurantAPI.IntegrationTests/RestaurantsControllerTests.cs
+++ RestaurantAPI.IntegrationTests/RestaurantsControllerTests.cs
@@ -15,7 +15,7 @@ namespace RestaurantAPI.IntegrationTests
     public class RestaurantsControllerTests : IntegrationTest
     {
         [Fact]
-        public async Task GetRestaurants_ReturnsNotEmptyResponse()
+        public async Task GetRestaurants_Should_ReturnListOfRestaurants()
         {
             //Arrange
             await AuthenticateAsync();
@@ -26,6 +26,46 @@ namespace RestaurantAPI.IntegrationTests
             //Assert
             response.StatusCode.Should().Be(HttpStatusCode.OK);
             (await response.Content.ReadFromJsonAsync<IEnumerable<RestaurantDto>>()).Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public async Task PostRestaurant_Should_CreateNewRestaurant()
+        {
+            //Arrange
+            await AuthenticateAsync();
+
+            var newRestaurant = new RestaurantForCreationDto()
+            {
+                Name = "Test restaurant",
+                Description = "Test description",
+                Category = "Test category",
+                HasDelivery = true,
+                ContactEmail = "test@test.com",
+                ContactNumber = "123456789",
+                Address = new AddressDto()
+                {
+                    City = "Test city",
+                    PostalCode = "12-345",
+                    Street = "Test street"
+                }
+            };
+
+            //Act
+            var response = await TestClient.PostAsJsonAsync("api/restaurants", newRestaurant);
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+
+            var returnedRestaurant = await response.Content.ReadAsAsync<RestaurantDto>();
+
+            returnedRestaurant.Name.Should().Be(newRestaurant.Name);
+            returnedRestaurant.Description.Should().Be(newRestaurant.Description);
+            returnedRestaurant.HasDelivery.Should().Be(newRestaurant.HasDelivery);
+            returnedRestaurant.ContactEmail.Should().Be(newRestaurant.ContactEmail);
+            returnedRestaurant.ContactNumber.Should().Be(newRestaurant.ContactNumber);
+            returnedRestaurant.Address.City.Should().Be(newRestaurant.Address.City);
+            returnedRestaurant.Address.Street.Should().Be(newRestaurant.Address.Street);
+            returnedRestaurant.Address.PostalCode.Should().Be(newRestaurant.Address.PostalCode);
         }
     }
 }
