75b443b27e66900cabd0952f60388d0f14986c4d
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-06-02 16:45:55 +0200

    Add name proprty to DishesResourceParameters

--- src/Domain/ResourceParameters/DishesResourceParameters.cs
+++ src/Domain/ResourceParameters/DishesResourceParameters.cs
@@ -1,10 +1,15 @@
 ﻿namespace Domain.ResourceParameters
 {
     /// <summary>
-    ///     Dishes resource parameters with SortBy, SortDirection, SearchQuery, PageNumber and MaximumPrice fields
+    ///     Dishes resource parameters with SortBy, SortDirection, SearchQuery, PageNumber, Name and MaximumPrice fields
     /// </summary>
     public class DishesResourceParameters : ResourceParameters
     {
+        /// <summary>
+        /// The name of the dish
+        /// </summary>
+        public string Name { get; set; }
+
         /// <summary>
         ///     Maximum price of the dish
         /// </summary>
